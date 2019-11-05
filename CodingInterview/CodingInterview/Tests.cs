218cab163abe5d5c458b509eb7c2cae0af6e2436
Andrzej <xurten@gmail.com>
2019-11-05 20:58:34 +0100

    Added second test for duplicates

--- CodingInterview/CodingInterview/Tests.cs
+++ CodingInterview/CodingInterview/Tests.cs
@@ -16,6 +16,27 @@ namespace CodingInterview
             return numbers.ToArray();
         }
 
+        int[] LeaveTwoDuplicates(int[] arrayOfNumbers)
+        {
+            var numbers = new HashSet<int>();
+            var numbersWithTwoDuplicates = new List<int>();
+            for (int index = 0; index < arrayOfNumbers.Length; index++)
+            {
+                numbers.Add(arrayOfNumbers[index]);
+            }
+
+            for (int index = 0; index < numbers.Count; index++)
+            { 
+                var actualValue = numbers.ElementAt(index);
+                if (arrayOfNumbers.Where(element => element == actualValue).Count() >= 2)
+                {
+                    numbersWithTwoDuplicates.Add(actualValue);
+                }
+                numbersWithTwoDuplicates.Add(actualValue);
+            }
+            return numbersWithTwoDuplicates.ToArray();
+        }
+
         [Test]
         public void RemoveDuplicatesFromSortedArray_1()
         {
@@ -25,6 +46,17 @@ namespace CodingInterview
             var sortedArrayWithoutDuplicatesCalculated = RemoveDuplicates(sortedArrayWithDuplicates);
 
             Assert.AreEqual(expectedSortedArrayWithoutDuplicates, sortedArrayWithoutDuplicatesCalculated);
+        }
+
+        [Test]
+        public void RemoveDuplicatesFromSortedArray_2()
+        {
+            var sortedArrayWithDuplicates = new int[] { 1, 3, 3, 3, 6, 8, 8, 9, 9, 9 };
+            var expectedSortedArrayWithTwoDuplicates = new int[] { 1, 3, 3, 6, 8, 8, 9, 9 };
+
+            var sortedArrayWithTwoDuplicatesCalculated = LeaveTwoDuplicates(sortedArrayWithDuplicates);
+
+            Assert.AreEqual(expectedSortedArrayWithTwoDuplicates, sortedArrayWithTwoDuplicatesCalculated);
         }
     }
 }
