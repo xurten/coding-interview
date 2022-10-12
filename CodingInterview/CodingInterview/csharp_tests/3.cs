fd7221f62051f06abcbfa14e5bbf733c4fe2f412
Andrzej <xurten@gmail.com>
2022-10-12 20:53:35 +0200

    Add third test

---
+++ CodingInterview/CodingInterview/csharp_tests/3.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+//Task 3
+// Write a program to calculate fibonacci
+namespace CodingInterview
+{
+    public class Class3
+    {
+
+        private static int GetFibonacciRecursive(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return GetFibonacciRecursive(n - 1) + GetFibonacciRecursive(n - 2);
+        }
+
+        private static int GetFibonacciIterative(int n)
+        {
+            var fibonacci_first = 0;
+            var fibonacci_second = 1;
+            foreach (var i in Enumerable.Range(1, n))
+            {
+                fibonacci_first = fibonacci_second;
+                fibonacci_second = fibonacci_first + fibonacci_second;
+            }
+            return fibonacci_first;
+        }
+
+        [Test]
+        public void CheckFibonacciRecursiveSmall()
+        {
+            var Expected = 13;
+            var n = 7;
+            Assert.AreEqual(GetFibonacciRecursive(n), Expected);
+        }
+
+        [Test]
+        public void CheckFibonacciRecursiveBigger()
+        {
+            var Expected = 20;
+            var n = 6765;
+            Assert.AreEqual(GetFibonacciRecursive(n), Expected);
+        }
+
+        [Test]
+        public void CheckFibonacciIterativeSmall()
+        {
+            var Expected = 13;
+            var n = 7;
+            Assert.AreEqual(GetFibonacciIterative(n), Expected);
+        }
+
+        [Test]
+        public void CheckFibonacciIterativeBigger()
+        {
+            var Expected = 20;
+            var n = 6765;
+            Assert.AreEqual(GetFibonacciIterative(n), Expected);
+        }
+    }
+
+}
---
+++ python_tests/test_3.py
@@ -0,0 +1,42 @@
+# Task 3
+# Write a program to calculate fibonacci
+
+# Implementation
+def get_fibonacci_recursive(n: int) -> int:
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return get_fibonacci_recursive(n - 1) + get_fibonacci_recursive(n - 2)
+
+
+def get_fibonacci_iterative(n: int) -> int:
+    fibonacci_first, fibonacci_second = 0, 1
+    for i in range(1, n+1):
+        fibonacci_first, fibonacci_second = fibonacci_second, fibonacci_first + fibonacci_second
+    return fibonacci_first
+
+
+# Test Section
+def test_fibonacci_recursive_small():
+    n = 7
+    expected_value = 13
+    assert get_fibonacci_recursive(n) == expected_value
+
+
+def test_fibonacci_recursive_bigger():
+    n = 20
+    expected_value = 6765
+    assert get_fibonacci_recursive(n) == expected_value
+
+def test_fibonacci_iterative_small():
+    n = 7
+    expected_value = 13
+    assert get_fibonacci_iterative(n) == expected_value
+
+
+def test_fibonacci_iterative_bigger():
+    n = 20
+    expected_value = 6765
+    assert get_fibonacci_iterative(n) == expected_value
