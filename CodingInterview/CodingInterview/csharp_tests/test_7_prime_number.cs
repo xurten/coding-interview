9996e0f850b82e1c43adb4a38063c4fb988a28dd
Andrzej <xurten@gmail.com>
2023-01-08 20:17:37 +0100

    Add prime test

---
+++ CodingInterview/CodingInterview/csharp_tests/test_7_prime_number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodingInterview
+{
+    public class Class7
+    {
+        public static bool Is_Prime(int number)
+        {
+            var prime_status = true;
+            for(var k = 2;k < Math.Round((decimal)number / 2); k++)
+            {
+                if(number % k == 0)
+                {
+                    prime_status = false;
+                    break;
+                }
+            }
+            return prime_status;
+        }
+
+        [Test]
+        [TestCase(13, true)]
+        [TestCase(2000, false)]
+        public void CheckIsPrime(int input, bool expected_value)
+        {
+            Assert.AreEqual(Is_Prime(input), expected_value);
+        }
+    }
+}
+
---
+++ python_tests/test_7_prime_number.py
@@ -0,0 +1,18 @@
+# Task 7
+# Write a program to check if a number is a prime number
+
+def is_prime(number):
+    prime_status = True
+    for k in range(2, round(number / 2)):
+        if number % k == 0:
+            prime_status = False
+            break
+    return prime_status
+
+
+def test_prime_number():
+    assert is_prime(13)
+
+
+def test_not_prime_number():
+    assert not is_prime(2000)
